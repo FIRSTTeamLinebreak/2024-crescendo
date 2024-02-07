bd6804439d7e788a6f45cd3e66845e51eef1c378
Team Macbook <programming@teamlinebreak.com>
2024-02-06 20:43:34 -0700

    Forgot Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -80,6 +80,14 @@ public final class Constants {
 
                 public static final double kT = 0.01;
             }
+
+            public static final class Elevator {
+                public static final double kP = 0.1;
+                public static final double kI = 0.0;
+                public static final double kD = 0.0;
+
+                public static final double kT = 0.0;
+            }
         }
 
         public static final HolonomicPathFollowerConfig HolonomicConfig =
